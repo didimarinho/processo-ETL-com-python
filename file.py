d41984a5c63c2b320eb27389edaf146823033829
diegomarinho <diegosignature@gmail.com>
2023-02-08 21:06:49 -0300

    processo ETL

--- file.py
+++ file.py
@@ -5,21 +5,25 @@ from datetime import datetime
 
 
 
-tmpfile    = "temp.tmp"               # file used to store all extracted data
-logfile    = "logfile.txt"            # all event logs will be stored in this file
-targetfile = "transformed_data.csv"   # file where transformed data is stored
 
 
-#! organizando os arquivos csv em data frame
+
+'''
+VAMOS CRIAR UMA FUNÇÃO STANDARD DE LEITURA DE ARQUIVO CSV QUE VAMOS USAR
+COMO PARTE DE OUTRA FUNÇÃO
+'''
 def extract_from_csv(file_to_process):
     dataframe = pd.read_csv(file_to_process)
     return dataframe
     
     
-    
 
 
-#! organizando os arquivos json em data frame
+
+'''
+VAMOS CRIAR UMA FUNÇÃO STANDARD DE LEITURA DE ARQUIVO JSON QUE VAMOS USAR
+COMO PARTE DE OUTRA FUNÇÃO
+'''
 def extract_from_json(file_to_process):
     dataframe = pd.read_json(file_to_process,lines=True)
     return dataframe
@@ -27,7 +31,10 @@ def extract_from_json(file_to_process):
 
 
 
-#! organizando os arquivos xml em data frame
+'''
+VAMOS CRIAR UMA FUNÇÃO STANDARD DE LEITURA DE ARQUIVO XML QUE VAMOS USAR
+COMO PARTE DE OUTRA FUNÇÃO
+'''
 def extract_from_xml(file_to_process):
     dataframe = pd.DataFrame(columns=["name", "height", "weight"])
     tree = ET.parse(file_to_process)
@@ -42,9 +49,10 @@ def extract_from_xml(file_to_process):
 
 
 
-
-#! criando um adata frame vazio com as colunas 'name', 'height', e 'weight' e extraindo as info
-#! dos arquivos csv, json e xml
+'''
+VAMOS CRIAR UM DATA FRAME VAZIO COM AS COLUNAS NAME, HEIGHT E WEIGHT E VAMOS USAR AS FUNÇÕES QUE CRIAMOS 
+ACIMA PARA EXTRAIR OS ARQUIVOS E ISERIR NO DATA FRAME VAZIO CHAMADO EXTRACTED_DATA
+'''
 def extract():
     extracted_data = pd.DataFrame(columns=['name','height','weight'])
     
@@ -65,7 +73,9 @@ def extract():
 
 
 
-#! convertendo as unidades de medidas de um dataframe
+'''
+VAMOS CRIAR UMA FUNÇÃO STANDART PARA USAR MAIS TARDE QUE VAI TRANSFORMAR AS MEDIDAS 
+'''
 def transform(data):
         # converte de polegadas para metros
         data['height'] = round(data.height * 0.0254,2)
@@ -77,39 +87,75 @@ def transform(data):
       
 
 
-
+'''
+VAMOS CRIAR UMA FUNÇÃO STANDARD PARA CRIAR UM ARQUIVO CSV EM NOSSO 
+DIRETÓRIO COM AS INFORMAÇÕES DAS TRANSFORMAÇÕES AUTOMATICAMENTE
+'''
 def load(targetfile,data_to_load):
     data_to_load.to_csv(targetfile)  
-    
 
-#! Criando um arquivo txt com os logs
+
+
+ 
+'''
+VAMOS CRIAR OS LOGS DE INFORMAÇÃO DE CADA PROCESSO DO ETL
+'''
 def log(message):
     timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
     now = datetime.now() # get current timestamp
     timestamp = now.strftime(timestamp_format)
     with open("logfile.txt","a") as f:
         f.write(timestamp + ',' + message + '\n')    
-        
-        
-        
+
+
+
+
+'''
+LOG DE INICIO DO PROCESSO ETL
+'''       
 log("ETL Job Started")
 
+
+
+'''
+VAMOS COMEÇAR A EXTRAÇÃO DOS DADOS
+'''
 log("Extract phase Started")
 extracted_data = extract()
 
+
+
+'''
+INFORMAÇÃO DE EXTRÇÃO ATRAVÉS DO LOG
+'''
 log("Extract phase Ended")
 extracted_data
 
+
+'''
+TRANSFORMANDO OS DADOS DE HEIGHT E WEIGHT
+'''
 log("Transform phase Started")
 transformed_data = transform(extracted_data)
 
+'''
+INFORMAÇÃO DE LOG DA TRANSFORMAÇÃO DOS DADOS
+'''
 log("Transform phase Ended")
 transformed_data 
 
+
+'''
+VAI DAR INFORMAÇÃO DE LOG E VAI CRIAR O ARQUIVO CSV
+COM OS DADOS TRANSFORMADOS E CARREGADOS
+'''
 log("Load phase Started")
 load(targetfile,transformed_data)
 
 
-log("Load phase Ended")
 
+'''
+INFORMAÇÃO DE LOG PARA FINALIZAÇÃO DO ETL
+'''
+log("Load phase Ended")
 log("ETL Job Ended")
